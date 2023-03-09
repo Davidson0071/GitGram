fdb13e31253f678e95b80c733701a9aa427df152
Davidson0071 <87654981+Davidson0071@users.noreply.github.com>
2023-03-09 03:03:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "6079084750:AAHTEd_yk2caumzfBmkH02pyqSp-nQV118o"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "GitTelBot"
 
 # If you forked the source code, grab your repo's link and paste it below!
 GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
